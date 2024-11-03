b85a84dee1a5ea7382f2278852c52dc7487f84aa
salpakak <salpakak@gmail.com>
2024-11-04 02:45:27 +0300

    feat: solve getFloatOnString task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -445,8 +445,9 @@ function isInteger(number) {
  * '4.567abcdefgh' => 4.567
  * 'abcdefgh'      => NaN
  */
-function getFloatOnString(/* str */) {
-  throw new Error('Not implemented');
+function getFloatOnString(str) {
+  const num = Number.parseFloat(str);
+  return Number.isNaN(num) ? NaN : num;
 }
 
 /**
